9f00cba34aa96cfbb8d9d313fa0f1fc2c26c76e5
hannanlukas <hannanlukas@gmail.com>
2025-06-11 15:16:41 +0100

    added stuff
    stuff

---
+++ memdothis-lib/include/memdothis.h
@@ -0,0 +1,12 @@
+#pragma once
+
+class memdothis
+{
+	int x_;
+	int y_;
+	int z_;
+
+	memdothis(int x, int y, int z);
+	~memdothis() = default;
+
+};
---
+++ memdothis-lib/src/memdothis.cpp
@@ -0,0 +1,8 @@
+#include "memdothis.h"
+
+memdothis::memdothis(int x, int y, int z)
+	:
+x_(x), y_(y), z_(z)
+{
+
+}
